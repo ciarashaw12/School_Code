cb54fdc520be2b2ab0476e4a1d1cc53fb2371917
Ciara Shaw <ciaras@smu.edu>
2018-02-13 14:28:53 -0600

    Starting Project1

---
+++ CSE-3353/Project1/Airplanedatagenerator.js
@@ -0,0 +1,26 @@
+/*Project 1
+Author: Ciara Shaw
+*/
+/*Starting Chance*/
+
+//Loading Chance
+var Chance = require('chance');
+
+//Instantiate Chance so it can be used
+var chance = new Chance();
+
+var jsexport = require('jsonfile');
+var fs = requie('fs');
+
+//Starting to create an Array Of Objects
+{
+    "Airplane_data" = []
+    
+}
+
+//Input data into Array
+for(i = 0; i < 100; i++)
+{
+
+}
+
