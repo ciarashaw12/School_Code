4572f9ac799b49d226c2c124e40c4a9df58f0716
Ciara Shaw <ciaras@smu.edu>
2018-02-15 15:20:15 -0600

    Completed data

--- CSE-3353/Project1/Airplanedatagenerator.js
+++ CSE-3353/Project1/Airplanedatagenerator.js
@@ -13,10 +13,23 @@ var chance = new Chance();
 //Starting to create an Array Of Objects
  var  Airplane_data = [];
 
-//Creating the object for array
+//Adding the info into the array.
 
 for(i = 0; i < 100; i++)
 {
-
-    Airplane_data.push({FlightNumber:chance.string(), x:chance.floating(), y:chance.floating()});
+    let char = chance.string({pool:'ABCDEFGHIJKLMNOPQRSTUVWXYZ', length:2});
+    let num  = chance.string({length:4, pool:'1234567890'});
+    let name = char + num;
+    Airplane_data.push({FlightNumber:name, 
+    x:chance.floating({min:-10, max: 10, fixed:1}), 
+    y:chance.floating({min:-10, max:10, fixed:1})});
 }
+
+for(j = 0; j < 100; j++)
+{
+    console.log(Airplane_data[j]);
+}
+
+
+
+//Writing out to a json file.
