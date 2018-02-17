3586f08a9a3e4cb0232cd9f6fe3afbf29e161464
Ciara Shaw <ciaras@smu.edu>
2018-02-17 03:10:07 -0600

    Fixed organization of airplane_map.json file

--- CSE-3353/Project1/Airplanedatagenerator.js
+++ CSE-3353/Project1/Airplanedatagenerator.js
@@ -16,10 +16,6 @@ var chance = new Chance();
  //Array for plotting graph
  var Airplane_gdata = [];
 
-
- 
-//creating new airplane object;
-
 //Adding the info into the array.
 
 for(i = 0; i < 100; i++)
@@ -37,10 +33,6 @@ for(i = 0; i < 100; i++)
         y: chance.floating({min:-10,max:10, fixed:4}),
     }
     Airplane_data.push(Airplane);
-
-    //Airplane_data.push({Airplane.AirplaFlightNumber:name, 
-    //x:chance.floating({min:-10, max: 10, fixed:4}), 
-    //y:chance.floating({min:-10, max:10, fixed:4})} );
 }
 
 //Writing out to a json file.
@@ -58,32 +50,41 @@ for(i = 0; i < 100; i++)
 //It will export a url in the console when fucntion is called
 //In order to start testing my brute force algorithm
 
-createGraph = function()
+createGraph = function(x,y)
 {
-    var plotly = require('plotly')("CiaraShaw","zukAhlJsEm9A99MH4c4W");
-    for(i = 0; i < 100; i++)
+
+    y.mode = null;
+    y.type =null;
+
+    let intg = 0
+
+    for(i = 0;i < 100; i++)
     {
-        //Giving the first two letters
-        let char = chance.string({pool:'ABCDEFGHIJKLMNOPQRSTUVWXYZ', length:2});
-        //the last four digits for flightnumber 
-        let num  = chance.string({length:4, pool:'1234567890'});
-        let name = char + num;
-        Airplane_gdata.push({FlightNumber:name, 
-        x:chance.floating({min:-10, max: 10, fixed:4}), 
-        y:chance.floating({min:-10, max:10, fixed:4}),
-        mode: "markers",
-        type: "scatter"});
-    }
+        x[i].mode = "markers";
+        x[i].type = "scatter";
+    } 
+
+     var plotly = require('plotly')("CiaraShaw","zukAhlJsEm9A99MH4c4W");
+
+    
+
+
+
+    fs.writeFile("./test_map.json", JSON.stringify(x, null, '\t'), 'utf8', (err) => {
+        if (err) {
+            console.error(err);
+            return;
+        };
+        console.log("test File has been created");
+    });
 
     var graphOptions = {filename: "line-scatter", fileopt: "overwrite"};
-    plotly.plot(Airplane_gdata, graphOptions, function (err, msg) {
-    console.log(msg);
-});
 
-
+    plotly.plot(x, graphOptions, function (err, msg) {
+    console.log(msg)});
 }
 
-createGraph();
+createGraph(Airplane_data,Airplane);
 
 
 
