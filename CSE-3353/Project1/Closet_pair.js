ac0070efee57354d481a8d7c8ee7654e04bdbce9
Ciara Shaw <ciaras@smu.edu>
2018-02-19 17:38:59 -0600

    Brute force Function works

--- CSE-3353/Project1/Closet_pair.js
+++ CSE-3353/Project1/Closet_pair.js
@@ -4,22 +4,47 @@ var data = require('./airplane_map.json');
 
 //Brute Force Implentation
 
-getDistance = function(x1,x2,y1,y2)
-{
 
-    return dist = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2)); 
+getDistance = function(p1,q1)
+{
+    return dist = Math.sqrt(Math.pow(p1.x - q1.x, 2) + Math.pow(p1.y - q1.y, 2)); 
 }
+
+let Airplane1;
+let Airplane2;
+let distance;
 
 BruteForce = function(data)
 {
-    for(i = 0; i < data.length; i++)
+    let Airplane1;
+    let Airplane2;
+    let distance;
+    let min = 0;
+    for(i = 0; i < data.length; ++i)
     {
-        for(j = i+ 1; j < data.length; j++)
+        for(j = i + 1; j < data.length; ++j)
         {
-            
+            let dist = getDistance(data[i],data[j])
+            //Setting the intial first distance calcuated between two points
+            if(i === 0 && j === 1)
+            {
+                min = dist;
+            }
+            else if(dist < min)
+            {
+                let Airplane1 = data[i].FlightNumber;
+                let Airplane2 = data[j].FlightNumber;
+                min = dist;
+
+            }
+           
         }
+
+        
     }
+
+    
+    console.log(Airplane1, " ", Airplane2, " ", min);
 }
 
-
-console.log(getDistance(2,5,8,9));
+BruteForce(data);
