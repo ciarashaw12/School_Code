447383544abf4e7c98fe78d6adb1f5139ed77357
Ciara Shaw <ciaras@smu.edu>
2018-02-19 18:16:10 -0600

    Brute force Function works

--- CSE-3353/Project1/Closet_pair.js
+++ CSE-3353/Project1/Closet_pair.js
@@ -19,27 +19,43 @@ BruteForce = function(data)
     {
         for(j = i + 1; j < data.length; ++j)
         {
+            //assign distance based of the x and y coordinates of the array
             let dist = getDistance(data[i],data[j])
             //Setting the intial first distance calcuated between two points
             if(i === 0 && j === 1)
             {
                 min = dist;
             }
-            else if(dist < min)
+            else if(dist < min) //If the dist is smaller than the prev assigned dist.Then the min and the two airplanes will be updated.
             {
-                var Airplane1 = data[i].FlightNumber;
-                var Airplane2 = data[j].FlightNumber;
+                var Airplane1_name = data[i].FlightNumber;
+                var Airplane1_x = data[i].x;
+                var Airplane1_y = data[i].y;
+                var Airplane2_name = data[j].FlightNumber;
+                var Airplane2_x = data[j].x;
+                var Airpane2_y = data[j].y;
                 min = dist;
 
             }
            
         } 
     }
+    //Returns a object of the results from the algorithm
+    console.log("Brute Force Results");
     return{
-        Airplane1,
-        Airplane2,
-        min
+        "This is Airpane1":Airplane1_name,
+        "X coordinate":Airplane1_x,
+        "Y coordinate": Airplane1_y,
+        "This is Airplane 2":Airplane2_name,
+        "X2 coordinate": Airplane2_x,
+        "Y2 coordinate": Airpane2_y,
+        "Distance":min
     };
+}
+
+sortedBruteForce = function(data)
+{
+
 }
 
 console.log(BruteForce(data));
